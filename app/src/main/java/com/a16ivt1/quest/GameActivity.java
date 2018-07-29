67f0a3fdb1f99edcdbcffb182041a64ba49c04c0
Trpoal <trpoal@mail.ru>
2018-07-30 00:56:50 +0300

    design

--- app/src/main/java/com/a16ivt1/quest/GameActivity.java
+++ app/src/main/java/com/a16ivt1/quest/GameActivity.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
